62ff0bc767fd9e8771431f789b883150b6d7499c
Sarang <sarang@gmail.com>
2022-12-21 11:01:31 +0530

    this commit is for localdate

--- GitPrject/src/Com/git/HelloWorld.java
+++ GitPrject/src/Com/git/HelloWorld.java
@@ -1,11 +1,14 @@
 package Com.git;
 
 import java.sql.Date;
+import java.time.LocalDate;
 
 public class HelloWorld {
 public static void main(String[] args) {
 	System.out.println("Hello World");
 	Runtime r = Runtime.getRuntime();
 	System.out.println(r);
+	LocalDate d = LocalDate.now();
+	System.out.println(d);
 }
 }
