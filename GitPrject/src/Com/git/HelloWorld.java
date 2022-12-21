c3f48391d67264a146548d0977ae967060567915
Sarang <sarang@gmail.com>
2022-12-21 11:04:08 +0530

    This commit is for Date

--- GitPrject/src/Com/git/HelloWorld.java
+++ GitPrject/src/Com/git/HelloWorld.java
@@ -7,5 +7,7 @@ public static void main(String[] args) {
 	System.out.println("Hello World");
 	Runtime r = Runtime.getRuntime();
 	System.out.println(r);
+	Date D = new Date (2023);
+	System.out.println(D);
 }
 }
