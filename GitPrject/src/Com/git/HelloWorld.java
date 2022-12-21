9548cd905bb134e2791f5ceb649c2f1e0243822f
Sarang <sarang@gmail.com>
2022-12-21 10:49:38 +0530

    this commit on runtime update code

--- GitPrject/src/Com/git/HelloWorld.java
+++ GitPrject/src/Com/git/HelloWorld.java
@@ -1,7 +1,11 @@
 package Com.git;
 
+import java.sql.Date;
+
 public class HelloWorld {
 public static void main(String[] args) {
 	System.out.println("Hello World");
+	Runtime r = Runtime.getRuntime();
+	System.out.println(r);
 }
 }
